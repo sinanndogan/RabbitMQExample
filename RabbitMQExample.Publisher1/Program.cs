d4bd0452e36d271e5268fe54c525178003f224d4
Sinan <sinnandogann@outlook.com>
2023-04-06 16:54:48 +0300

    Add project files.

---
+++ RabbitMQExample.Publisher1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+//Önce factory sınfı ile rabbitMQ bağlantısı oluşturulur
+//RabitMQ sunucusuna bağlandık.
+ConnectionFactory factory = new ();
+factory.Uri = new("amqps://vgoeohfm:NmkSmsxycy31BqcSuJ7Me8qFIdR0E6Y_@shrimp.rmq.cloudamqp.com/vgoeohfm");
+
+
+//Bağlantıyı aktfileştirme ve kanal açma
+
+using IConnection connection = factory.CreateConnection();  //bağlantı
+using IModel channel = connection.CreateModel();    //kanal 
+
+
+//Kuyruk Queue Oluşturma 
+//kuyruk adı example-queue
+channel.QueueDeclare(queue:"example-queue",exclusive:false);
+
+
+//Queue'ya Mesaj Gönderme
+//RabbitMQ kuyruğa atacağı mesajarı byte türünden kabul etmektedir.Haliyle mesajları bizim byte dönüştürmemiz gerekecektir.
+
+byte [] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+Console.Read();
