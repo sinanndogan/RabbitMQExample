c8ac13bf75a5850b4a8c71f5f8f76e607785df65
Sinan <sinnandogann@outlook.com>
2023-04-07 16:47:22 +0300

    Eklemler Yapıldı

---
+++ RabbitMQ.Direct.Exchange.Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vgoeohfm:NmkSmsxycy31BqcSuJ7Me8qFIdR0E6Y_@shrimp.rmq.cloudamqp.com/vgoeohfm");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//1.Adım
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+//2.Adım
+string queueName=channel.QueueDeclare().QueueName;
+
+//3.Adım
+channel.QueueBind(queue:queueName, exchange: "direct-exchange-example", routingKey: "direct-queue-example");
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
+
+
+//1.Adım : Publisher'da ki exchange ile birebir aynı isim ve type'a sahip sahip bir exhange tanımlanmalıdır!
+
+//2.Adım : Publisher tarafından routing key'de bulunan değerdeki kuyruğa gönderlien mesajları kendi oluşturduğumuz kuyruğa yönlendirerek tüketmemiz gerekmektedir. Bunun için öncellikle bir kuyruk oluşturulmalıdır.
+
+
+//3.Adım : 
---
+++ RabbitMQ.Direct.Exchange.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri= new("amqps://vgoeohfm:NmkSmsxycy31BqcSuJ7Me8qFIdR0E6Y_@shrimp.rmq.cloudamqp.com/vgoeohfm");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel =connection.CreateModel();
+
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "direct-exchange-example",routingKey:"direct-queue-example",body:byteMessage);
+}
+Console.Read();
+
+
+//Biz burda herhangi bir kuyruk oluşturmadık burda exchange type oluşturduk ve onun üzerinden consumer tarafında oluşturulacak olan kuyruk ismine gideceğini routingkey ile belirtmiş olduk 
