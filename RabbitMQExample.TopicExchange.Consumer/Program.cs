24332f2266bf744f2bda671d19e23dfc5c5bf602
Sinan <sinnandogann@outlook.com>
2023-04-10 11:48:56 +0300

    Topic davranış örnek

---
+++ RabbitMQExample.TopicExchange.Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vgoeohfm:NmkSmsxycy31BqcSuJ7Me8qFIdR0E6Y_@shrimp.rmq.cloudamqp.com/vgoeohfm");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+Console.Write("Dinlenecek topic formatını belirtiniz : ");
+string topic = Console.ReadLine();
+
+string queueName=channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queue: queueName, exchange: "topic-exchange-example", routingKey: topic);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQExample.TopicExchange.Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vgoeohfm:NmkSmsxycy31BqcSuJ7Me8qFIdR0E6Y_@shrimp.rmq.cloudamqp.com/vgoeohfm");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    Console.Write(" Mesajın gönderileceği Topic formatını  belirtiniz : ");
+    string topic = Console.ReadLine();
+    channel.BasicPublish(exchange: "topic-exchange-example", routingKey: topic, body: message);
+}
+
+//Not: RoutingKey exchange çeşidine göre davranışını şekkilendiren bir yapıya sahip.
+
+Console.Read();
