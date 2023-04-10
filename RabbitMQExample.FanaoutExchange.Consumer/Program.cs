719c1266c0df234c8c33483880949d7da23be0fe
Sinan <sinnandogann@outlook.com>
2023-04-10 10:18:33 +0300

    Fanout exchange eklemesi yapıldı

---
+++ RabbitMQExample.FanaoutExchange.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vgoeohfm:NmkSmsxycy31BqcSuJ7Me8qFIdR0E6Y_@shrimp.rmq.cloudamqp.com/vgoeohfm");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//1.Adım Exchange tanımla 
+channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
+
+Console.Write("Kuyruk Adını giriniz : ");
+string _queueName=Console.ReadLine();
+channel.QueueDeclare(queue: _queueName, exclusive:false);
+
+
+channel.QueueBind(queue: _queueName, exchange: "fanout-exchange-example",routingKey:string.Empty);
+
+
+//Mesajları okuma
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: _queueName, autoAck:true, consumer:consumer);
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQExample.FanaoutExchange.Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vgoeohfm:NmkSmsxycy31BqcSuJ7Me8qFIdR0E6Y_@shrimp.rmq.cloudamqp.com/vgoeohfm");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Mesajların bu exchange'e bind olmuş olan tüm kuyruklara gönderilmesini sağlar.Publisher mesajların gönderildiği kuyruk isimlerini dikkate almaz ve mesajları tüm kuyruklara gönderir
+channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
+
+//100 adet mesaj yayınlamak istiyorum 
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba{i}");
+    channel.BasicPublish(exchange: "fanout-exchange-example", 
+    routingKey:string.Empty,
+    body:message);
+}
+
+//routingkey : "" boş bir değer ya da string.Empty olabilir.
