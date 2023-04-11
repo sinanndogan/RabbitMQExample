cd9d2a53a2b439f41b242ad6907ed89ff32062ad
Sinan <sinnandogann@outlook.com>
2023-04-11 11:32:26 +0300

    RequestResponseDesign

--- MessageDesigns/RabbitMQExample.MessageDesigns.Consumer/Program.cs
+++ MessageDesigns/RabbitMQExample.MessageDesigns.Consumer/Program.cs
@@ -4,7 +4,7 @@ using RabbitMQ.Client.Events;
 using System.Text;
 
 ConnectionFactory factory = new();
-factory.Uri = new("amqps://vgoeohfm:NmkSmsxycy31BqcSuJ7Me8qFIdR0E6Y_@shrimp.rmq.cloudamqp.com/vgoeohfm");
+factory.Uri = new("localhost");
 
 using IConnection connection = factory.CreateConnection();
 using IModel channel =connection.CreateModel();
@@ -28,24 +28,77 @@ using IModel channel =connection.CreateModel();
 #region Publish/Subscribe (Pub/Sub) Tasarımı
 //Bu tasarım bir mesajın birçok tüketici tarafından işlenmesi gerektiği durumlarda kullanışlıdır. 
 
-string exchangeName = "example-pub-sub-exchange";
+//string exchangeName = "example-pub-sub-exchange";
 
-channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+//channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
 
-string queueName = channel.QueueDeclare().QueueName;
-channel.QueueBind(
-    queue: queueName,
-    exchange: exchangeName,
-    routingKey: string.Empty
-    );
+//string queueName = channel.QueueDeclare().QueueName;
+//channel.QueueBind(
+//    queue: queueName,
+//    exchange: exchangeName,
+//    routingKey: string.Empty
+//    );
+
+//EventingBasicConsumer consumer = new(channel);
+//channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+//consumer.Received += (sender, e) =>
+//{
+//    string message = Encoding.UTF8.GetString(e.Body.Span);
+//    Console.WriteLine(message);
+//};
+
+#endregion
+
+
+
+#region Work Queue(İş Kuyruğu) Tasarımı
+//Publisher tarafından yayınlanmış bir mesajın birden fazla consumer arsından yalnızca biri tarafınan tüketilmesi amaçlanmaktadır.Böylece mesajların işlenmesi sürecinde tüm consumer'lar aynı iş yüküne ve eşit görev dağılımına sahip olacaktırlar. 
+
+
+//herbir consumerin tek bir mesaj işlemesini  garanti altına almış olduk 
+//string queueName = "example-work-queue";
+
+//channel.QueueDeclare(queue: queueName,
+//    durable: false,
+//    exclusive: false,
+//    autoDelete: false);
+
+//EventingBasicConsumer consumer = new(channel);
+//channel.BasicConsume(queue: queueName, autoAck: true, consumer:consumer);
+
+////tüm consumerlar 1 adet mesaj okuyacak boyutu önemsiz 
+//channel.BasicQos(prefetchCount: 1, prefetchSize: 0, global: false);
+
+//consumer.Received += (sender, e) =>
+//{
+//    string message = Encoding.UTF8.GetString(e.Body.Span);
+//    Console.WriteLine(message);
+//};
+
+
+#endregion
+
+
+
+#region Request/Response Tasarımı
+string requestQueueName = "example-request-response-queue";
+
+channel.QueueDeclare(queue: requestQueueName, durable: false, exclusive: false, autoDelete: false);
 
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+channel.BasicConsume(queue: requestQueueName, autoAck: true, consumer: consumer);
 
 consumer.Received += (sender, e) =>
 {
-    string message = Encoding.UTF8.GetString(e.Body.Span);
+   string message =Encoding.UTF8.GetString(e.Body.Span);
     Console.WriteLine(message);
+    byte[] responseMessage = Encoding.UTF8.GetBytes($"İşlem tamamlandı : {message} ");
+    IBasicProperties properties =channel.CreateBasicProperties();
+    properties.CorrelationId=e.BasicProperties.CorrelationId;
+    channel.BasicPublish(exchange: string.Empty, routingKey: e.BasicProperties.ReplyTo,basicProperties:properties,body:responseMessage);
 };
-
 #endregion
+
+
+Console.Read();
--- MessageDesigns/RabbitMQExample.MessageDesigns.Publisher/Program.cs
+++ MessageDesigns/RabbitMQExample.MessageDesigns.Publisher/Program.cs
@@ -1,8 +1,9 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 ConnectionFactory factory = new();
-factory.Uri = new("amqps://vgoeohfm:NmkSmsxycy31BqcSuJ7Me8qFIdR0E6Y_@shrimp.rmq.cloudamqp.com/vgoeohfm");
+factory.Uri = new("localhost");
 
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
@@ -24,16 +25,83 @@ using IModel channel = connection.CreateModel();
 #region Publish/Subscribe (Pub/Sub) Tasarımı
 //Bu tasarım bir mesajın birçok tüketici tarafından işlenmesi gerektiği durumlarda kullanışlıdır.
 
-string exchangeName = "example-pub-sub-exchange";
+//string exchangeName = "example-pub-sub-exchange";
 
-channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+//channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
 
-for (int i = 0; i < 10; i++)
-{
-   await Task.Delay(200);
-byte[] message = Encoding.UTF8.GetBytes("merhba");
-channel.BasicPublish(exchange: exchangeName, routingKey: string.Empty, body: message);
-}
+//for (int i = 0; i < 10; i++)
+//{
+//   await Task.Delay(200);
+//byte[] message = Encoding.UTF8.GetBytes("merhba");
+//channel.BasicPublish(exchange: exchangeName, routingKey: string.Empty, body: message);
+//}
 
 
 #endregion
+
+
+
+#region Work Queue(İş Kuyruğu) Tasarımı
+//Publisher tarafından yayınlanmış bir mesajın birden fazla consumer arsından yalnızca biri tarafınan tüketilmesi amaçlanmaktadır.Böylece mesajların işlenmesi sürecinde tüm consumer'lar aynı iş yüküne ve eşit görev dağılımına sahip olacaktırlar. 
+
+//string queueName = "example-work-queue";
+
+//channel.QueueDeclare(queue: queueName,
+//    durable: false,
+//    exclusive: false,
+//    autoDelete: false);
+
+//for (int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(200);
+//    byte[] message =Encoding.UTF8.GetBytes("Merhaba"+i);
+//    channel.BasicPublish(exchange: string.Empty, routingKey: queueName, body: message);
+//}
+#endregion
+
+
+#region Request/Response Tasarımı
+
+string requestQueueName = "example-request-response-queue";
+
+channel.QueueDeclare(queue: requestQueueName, durable: false, exclusive: false, autoDelete: false);
+
+//geri dönüş kuyruğu
+string responseQueueName = channel.QueueDeclare().QueueName;
+
+
+//corelationıd olusturma
+
+string correlationId=Guid.NewGuid().ToString();
+
+#region Request mesajını oluşturma ve gönderme
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.CorrelationId = correlationId;
+properties.ReplyTo = requestQueueName;
+//replyto = bizim  publisher'e dönüş yolundaki kuyruk tanımlandı
+
+byte[] message = Encoding.UTF8.GetBytes("Deneme");
+channel.BasicPublish(exchange: string.Empty, routingKey: requestQueueName, body: message,
+    basicProperties: properties);
+
+#endregion
+
+
+#region response kuyruğu dinleme  consumer davranış sergilenecek yer 
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: responseQueueName,autoAck:true,consumer:consumer);
+
+consumer.Received += (sender, e) =>
+{
+	if (e.BasicProperties.CorrelationId==correlationId)
+	{
+        Console.WriteLine($"Response : {Encoding.UTF8.GetString(e.Body.Span)}");
+    }
+};
+#endregion
+
+
+
+#endregion
+
+Console.Read();
