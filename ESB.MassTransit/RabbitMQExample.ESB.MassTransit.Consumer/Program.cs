6269f637678cae3fe98088c4a938a790903f2741
Sinan <sinnandogann@outlook.com>
2023-04-11 15:14:16 +0300

    Enterprise Service Bus

---
+++ ESB.MassTransit/RabbitMQExample.ESB.MassTransit.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using RabbitMQExample.ESB.MassTransit.Consumer.Consumers;
+
+string rabbitMQUri = "yourlocalhost";
+
+
+string queueName = "example-queue";
+
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+
+    factory.ReceiveEndpoint(queueName, endpoint =>
+    {
+        endpoint.Consumer<ExampleMessageConsumer>();
+    });
+});
+
+
+await bus.StartAsync();
+
+Console.Read();
---
+++ ESB.MassTransit/RabbitMQExample.ESB.MassTransit.Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using MassTransit;
+using RabbitMQExample.ESB.MassTransit.Shared.Messages;
+
+string rabbitMQUri = "yourlocalhost";
+
+string queueName = "example-queue";
+
+
+IBusControl bus =Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+});
+
+//Gönderileecek endpoint mesaj buraya gidecek
+
+ISendEndpoint sendEndpoint = await  bus.GetSendEndpoint(new($"{rabbitMQUri}/{queueName}"));
+
+
+//Send tek bir kuyruğa tek bir hedefe mesaj göndermek için kullandığımız yapıdır.
+
+Console.Write("Gönderilecek Mesaj :  ");
+string message =Console.ReadLine();
+await sendEndpoint.Send<IMessage>(new ExampleMessage()
+{
+    Text = message
+}); 
+
+Console.Read();
